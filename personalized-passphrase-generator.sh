6c711eb300ade8baa48367d2c8b43cc8288dd981
hnwmn <newmaninfosecuk@gmail.com>
2025-07-28 23:57:47 +0100

    added code comments

--- personalized-passphrase-generator.sh
+++ personalized-passphrase-generator.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# Extracts unique alphabetic words from the input files
+# The awk '!seen[$0]++' pattern keeps only unique words
 extract_words() {
    cat "${input_files_array[@]}" | grep -oE '\b[[:alpha:]]+\b' | awk '!seen[$0]++' > "$output_file"
 }
 
 stopwords_file="stop_words.txt"
 
+# Generates a passphrase of a given length, excluding stop words
 generate_passphrase() {
    words_to_generate=$word_count
    generated_count=0
    generated_line="Generated passphrase: "
    while [ "$generated_count" -lt "$words_to_generate" ]; do
+        # Selects a random word from the unique words file using /dev/urandom as a random source for entropy
        word=$(shuf --random-source=/dev/urandom "$output_file" | head -n1)
-      if ! grep -qw "$(echo "$word" | LC_ALL=C tr '[:upper:]' '[:lower:]')" < <(LC_ALL=C tr '[:upper:]' '[:lower:]' < "$stopwords_file"); then
+        # Check if the chosen word is a stop word (like "the", "a", etc.)
+        # The comparison is case-insensitive as both the selected word and all stop words are converted to lowercase
+        # If the word is not found in the stop words list, it's appended to the generated passphrase and the counter for words to generate is decremented
+       if ! grep -qw "$(echo "$word" | LC_ALL=C tr '[:upper:]' '[:lower:]')" < <(LC_ALL=C tr '[:upper:]' '[:lower:]' < "$stopwords_file"); then
            generated_line="$generated_line$word "
            ((words_to_generate--))
        fi
@@ -31,6 +38,9 @@ fi
 
 read -p "Enter the path to your file(s): " input_files
 
+# Take the user's input string of file paths and turn it into an array
+# -r prevents backslashes from being treated as escape characters
+# -a reads the input into an array
 IFS=" " read -r -a input_files_array <<< "$input_files"
 
 for input_file in "${input_files_array[@]}"; do
@@ -53,12 +63,14 @@ while true; do
     fi
 done
 
+# Create a temporary file to hold our list of unique words
 output_file=$(mktemp)
 
 extract_words
 
 run=true
 
+# Main loop
 while true; do
   if [ "$run" = true ]; then
        generate_passphrase
